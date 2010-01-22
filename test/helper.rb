e930de75a6f2cdf9b796e85a02c55cb1bd548255
Elliot Winkler <elliot.winkler@gmail.com>
2010-01-21 20:46:29 -0600

    Tweak README

--- test/helper.rb
+++ test/helper.rb
@@ -6,13 +6,17 @@ gem 'mcmire-matchy'
 require 'matchy'
 gem 'mcmire-mocha'
 require 'mocha'
+gem 'mocha-protest-integration'
 require 'mocha-protest-integration'
 
 Protest.report_with :documentation
 Protest::Utils::BacktraceFilter::ESCAPE_PATHS.clear
 
+gem 'activesupport'
 require 'activesupport'
+gem 'actionpack'
 require 'action_view/helpers'
+# trick AssetTagHelper into thinking ActionController is loaded
 module ActionController
   class Base
     def self.asset_host
