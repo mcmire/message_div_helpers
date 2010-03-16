7378fad286b196bf52cc3fb35e6851ac4e2a69dc
Elliot Winkler <elliot.winkler@gmail.com>
2010-03-16 16:45:15 -0500

    Whoops, fix install.rb

--- install.rb
+++ install.rb
@@ -2,10 +2,10 @@
 images = File.dirname(__FILE__) + '/public/images/message_div_helpers'
 images_dir = "#{RAILS_ROOT}/public/images"
 FileUtils.cp_r(images, images_dir)
-puts "%s installed to %s." % [ Dir["#{images}/*"].map {|x| File.basename(x) }.to_sentence, images_dir]
+puts "These files were installed to %s: %s" % [ images_dir, Dir["#{images}/*"].map {|x| File.basename(x) }.join(", ") ]
 
 # Copy stylesheet to the project
 css_file = File.dirname(__FILE__) + '/public/stylesheets/message_div_helpers.css'
 stylesheets_dir = "#{RAILS_ROOT}/public/stylesheets"
 FileUtils.cp(css_file, stylesheets_dir)
-puts "%s installed to %s." % [ File.basename(css_file), stylesheets_dir ]
+puts "These files were installed to %s: %s" % [ stylesheets_dir, File.basename(css_file) ]
